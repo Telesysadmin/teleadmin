b21c7dbbcc7009accc96a5afda77ed0f08daf8d5
Сергей <pinigin.s@ispsystem.com>
2018-07-21 19:00:32 +0800

    compile

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#  strace gcc -s -Wl,-Bstatic -lpython3.5m -I /usr/include/python3.5m teleadmin_wsgi.c
+# gcc -s -Wl,-Bstatic -lpython3.5m -I /usr/include/python3.5m teleadmin_wsgi.c
 cp wsgi.py teleadmin_wsgi.pyx
 cython teleadmin_wsgi.pyx --embed
-gcc -Os -I /usr/include/python3.5m -o teleadmin_wsgi teleadmin_wsgi.c -lpython3.5m -lpthread -lm -lutil -ldl
+gcc -s -Wl,-Bstatic -lpython3.5m -I /usr/include/python3.5m teleadmin_wsgi.c
+#gcc -Os -I /usr/include/python3.5m -o teleadmin_wsgi teleadmin_wsgi.c -lpython3.5m -lpthread -lm -lutil -ldl
